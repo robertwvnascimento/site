7912452f7eace744fce933ce3177f4e389baa3c8
robertwvnascimento <robertwvnascimento@gmail.com>
2026-07-02 14:14:15 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.PATRIMON_CONFIG = {
-  apiKey: "AIzaSyBAy3_BNYaitL1MxtykVUcDyIr1592VkiU",
-  authDomain: "patrimon-da73f.firebaseapp.com",
-  projectId: "patrimon-da73f",
-  storageBucket: "patrimon-da73f.firebasestorage.app",
-  messagingSenderId: "986820316093",
-  appId: "1:986820316093:web:bd188eab264f657a39e0d4"
-};
